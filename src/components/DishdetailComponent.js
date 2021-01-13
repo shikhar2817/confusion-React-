35177afa2449aab17d4c8cba734ed1474e882358
Shikhar Singh <shikharsingh0076@gmail.com>
2021-01-13 10:27:28 +0530

    Screenshots Uploaded

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, Row, Col , BreadcrumbItem, Modal, ModalBody, ModalHeader, Form, FormGroup, Input ,Button, Label } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, Row , BreadcrumbItem, Modal, ModalBody, ModalHeader, Button, Label } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
